79779925942ea067c00ef22d3fdb43bb43ea17eb
Sofia Lobenko <sofia.lobenko@kos-ws05.exp.exactpro.com>
2022-02-08 07:27:10 +0300

    [TH2-2871] Add classes and converters to create tables

--- th2_common_utils/__init__.py
+++ th2_common_utils/__init__.py
@@ -14,4 +14,5 @@
 
 
 from .message_fields_access import *
-from .converters import message_to_dict, dict_to_message, dict_to_root_message_filter, message_to_typed_message
+from .converters import message_to_dict, dict_to_message, dict_to_root_message_filter, message_to_typed_message, \
+    message_to_table
